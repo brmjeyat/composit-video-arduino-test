e80fbe0e137567aeb982afcc4f87366d22ca63db
SalimF <salim1870@yahoo.com>
2020-03-11 02:13:19 +0200

    Create pcm-stm32f103.ino

---
+++ pcm-stm32f103.ino
@@ -0,0 +1,23 @@
+#include <HardwareTimer.h>
+#include "hex.h"  
+unsigned int p = 0            ;  
+HardwareTimer pwmtimer2(  1 ) ;      //  char Text [6];     
+void setup() {  
+  pinMode( PA8 , PWM)  ;
+  // Serial.begin(9600);
+ pwmtimer2.pause();
+ pwmtimer2.setPrescaleFactor( 1    ) ;
+// divid factor for main frequency , if  /2 = 36,000,000     .
+    pwmtimer2.setOverflow (   2250   ) ; //  1500   2000
+    pwmtimer2.refresh();
+    pwmtimer2.resume();    //pwmtimer2.();
+    pwmWrite( PA8  ,0   ) ;
+     }
+
+void loop() {
+char sample = pgm_read_byte(  &quack_wav[p++]   )  ;  // * 1.6   
+pwmWrite( PA8  , sample    )   ;   //  * 64 
+// 65536  ÷  4096
+if ( p == quack_wav_len ) {    p=0;     }  
+          delayMicroseconds( 100  );  //  62.5  ×2
+          }
